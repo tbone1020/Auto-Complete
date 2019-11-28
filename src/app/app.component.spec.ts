72ad4c24600d92f4f0b3c1266a5dcbf703b68725
Trent Teebken <trenttee1020@gmail.com>
2019-11-27 20:50:23 -0800

    Styled frontend, added unit testing, and got main functionality to work

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,23 @@
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  describe('AppComponent', () => {
-    it ('Should Exist', () => {
-        expect(AppComponent).toBeDefined();
-    })
+
+  let component: AppComponent = new AppComponent();
+  beforeEach(() => {
+
   });
+  it ('Should Exist', () => {
+      expect(component).toBeDefined();
+  });
+
+  it('Has autoCompleteResults', () => {
+    expect(component.autoCompleteResults).toBeDefined();
+  });
+
+  it('Checks if autoCompleteResults is an array', () => {
+    expect(Array.isArray(component.autoCompleteResults)).toBe(true);
+  });
+
+
 
 });
