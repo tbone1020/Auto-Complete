e390838b65fe6ce6973d60dad8c4c687357afbc6
Trent Teebken <trenttee1020@gmail.com>
2020-01-24 19:54:50 -0800

    README.md formatting, unit tests, and package.json updates

--- src/app/services/hashing.service.spec.ts
+++ src/app/services/hashing.service.spec.ts
@@ -1,14 +1,6 @@
 import { HashingService } from './hashing.service';
 
 describe('HashingService', () => {
-  let hashing: HashingService = new HashingService();
 
-  it ("Should exist", () => {
-    expect(hashing).toBeDefined();
-  })
 
-  it ("Should return valid char code", () => {
-    let letterCode = hashing.getLettersHashCode("a");
-    expect(letterCode).toEqual(0);
-  });
 });
